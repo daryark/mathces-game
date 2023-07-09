de67de04c439baed7f39ec54f9eb49d979b293a3
Dasha Yarkovska <d.yarkovska@unkd.com.ua>
2023-07-09 12:09:37 +0300

    add comments to describe formula descisions

--- src/optimalGameFn.ts
+++ src/optimalGameFn.ts
@@ -55,3 +55,5 @@ export default function optimalGameFn(total: number, AITotal: number): number | 
 //the percent of probability to predict better move is so insignificant, so It's not rational to spent resource to calculate the moves.
 
 //On the contrary, it's totally possible to force game to win anyway starting from 8.
+
+//Except one situation (when you have odd total and need to take some number of matches from box with 8 left). So just for last situation, I made 2moves *3 possible matches (9-14 cases), that lover the possibility of this situation at least a little bit. But still - strategic game mostly starts from 8.
