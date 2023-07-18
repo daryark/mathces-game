f58765681c4d42429a4b93d90820e6694e15a572
Dasha Yarkovska <yarkovska11@gmail.com>
2023-07-18 15:02:27 +0300

    test

--- src/optimalGameFn.ts
+++ src/optimalGameFn.ts
@@ -57,5 +57,3 @@ export default function optimalGameFn(total: number, AITotal: number): number | 
 //On the contrary, it's totally possible to force game to win anyway starting from 8.
 
 //Except one situation (when you have odd total and need to take some number of matches from box with 8 left). So just for last situation, I made 2moves *3 possible matches (9-14 cases), that lover the possibility of this situation at least a little bit. But still - strategic game mostly starts from 8.
-
-//
